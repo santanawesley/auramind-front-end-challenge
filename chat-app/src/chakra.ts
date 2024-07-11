ea7f95ac088c8cf3e23f3fe24352733c282373b5
Wesley Santana <wesley.santana@opah.com.br>
2024-07-11 11:53:41 -0300

    Replacing default structure with project structure

---
+++ chat-app/src/chakra.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from "@chakra-ui/react";
+
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+
+// Configuração global
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        bg: "#F8F8FF",
+        color: "#292929",
+        fontFamily: "Inter, sans-serif",
+        margin: 0,
+        padding: 0,
+      },
+    },
+  },
+});
+
+
+export default theme;
