d34c18b6e6592dbe11373fc3ecd738f22e556b3d
Wesley Santana <wesley.santana@opah.com.br>
2024-07-15 19:51:07 -0300

    Styling conversation titles in the sidebar

--- chat-app/src/components/atoms/ItemMenu.tsx
+++ chat-app/src/components/atoms/ItemMenu.tsx
@@ -3,18 +3,29 @@ import { Box, Text } from "@chakra-ui/react";
 interface ItemMenuProps {
   title: string;
   id: string;
+  idActive: string | null;
   idClicked: (id: string) => void;
 }
 
-const ItemMenu: React.FC<ItemMenuProps> = ({ title, id, idClicked }) => {
+const ItemMenu: React.FC<ItemMenuProps> = ({
+  title,
+  id,
+  idActive,
+  idClicked,
+}) => {
   return (
     <Box
       p="2"
-      borderBottom="1px solid"
-      borderColor="gray.200"
+      m="2"
+      ml="0"
+      borderRadius="12px"
       id={id}
       cursor="pointer"
       onClick={() => idClicked(id)}
+      color="#F8F8FF"
+      bg={idActive === id ? "gray.800" : "inherit"}
+      _hover={{ bg: "gray.600" }}
+      transition="background-color 0.2s ease"
     >
       <Text>{title}</Text>
     </Box>
--- chat-app/src/components/organisms/ChatList.tsx
+++ chat-app/src/components/organisms/ChatList.tsx
@@ -1,11 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 import ItemMenu from "@/components/atoms/ItemMenu";
 
 import { useAuth } from "@/contexts/AuthContext";
 
 const ChatList: React.FC = () => {
-  const { user, changeConversation } = useAuth();
+  const { user, changeConversation, idToChangeConversation } = useAuth();
 
   const chatsTitles = user?.conversations
     ?.filter((conversation) => conversation.title)
@@ -16,13 +16,41 @@ const ChatList: React.FC = () => {
     .reverse();
 
   return (
-    <Box overflowY="auto" h="full" w="full">
+    <Box
+      overflowY="auto"
+      mt="2"
+      mb="3"
+      pr="1.5"
+      h="full"
+      w="full"
+      sx={{
+        "&::-webkit-scrollbar": {
+          width: "8px",
+        },
+        "&::-webkit-scrollbar-thumb": {
+          background: "#888",
+          borderRadius: "4px",
+        },
+        "&::-webkit-scrollbar-thumb:hover": {
+          background: "#555",
+        },
+        "&::-webkit-scrollbar-track": {
+          background: "#f1f1f1",
+        },
+        scrollbarWidth: "thin",
+        scrollbarColor: "#888 #f1f1f1",
+      }}
+    >
+      <Text color="#f1f1f1" textAlign="center">
+        Histórico de conversas
+      </Text>
       {chatsTitles?.map((chat) => (
         <ItemMenu
           key={chat.id}
           title={chat.title}
           idClicked={changeConversation}
           id={chat.id}
+          idActive={idToChangeConversation}
         />
       ))}
     </Box>
